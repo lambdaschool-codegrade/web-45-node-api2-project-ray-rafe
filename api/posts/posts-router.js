03c59ee78a77a04d9473450d25599dc74f7e6fdf
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-08-31 13:59:20 -0600

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -113,4 +113,22 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+router.get("/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comments = await Posts.findPostComments(id);
+    if (comments.length) {
+      res.status(200).json(comments);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The comments information could not be retrieved" });
+  }
+});
+
 module.exports = router;
